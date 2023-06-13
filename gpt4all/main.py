7fd2a56d5fe0cd4e5798f043215c4457cc6c47b9
Wasserpuncher <70776301+Wasserpuncher@users.noreply.github.com>
2023-06-13 19:24:34 +0200

    Update main.py

--- gpt4all/main.py
+++ gpt4all/main.py
@@ -2,6 +2,6 @@ import gpt4all
 
 gpt = gpt4all.GPT4All("ggml-v3-13b-hermes-q5_1")
 
-msgs = [{"role": "user", "content": "Create a recipe for banana frappunchino"}] 
+msgs = [{"role": "user", "content": "Program me a website"}] 
 
 gpt.chat.completion(msgs)
